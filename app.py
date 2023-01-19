877554e0564c85c05d9a6073181c764af73305c3
Raihan Khan <iamrk98@gmail.com>
2023-01-19 20:44:32 +0530

    renamed from picasso to davinci

--- app.py
+++ app.py
@@ -3,26 +3,25 @@ from engine import artGen, uploadNFT, mintNFT
 import time
 import json
 
+
 def upload_and_mint():
-    try:
-        with st.spinner("Minting NFT..."):
-            if st.session_state.name and st.session_state.desc and st.session_state.mint_to_address:
-                cid = uploadNFT(st.session_state.img_url, st.session_state.prompt)
-                mint_info = mintNFT(st.session_state.name, st.session_state.desc, cid, st.session_state.mint_to_address)
-                st.success(f'NFT Minted successfully to {st.session_state.mint_to_address}')
-                res = json.loads(mint_info)
-                # st.json(res)
-                st.info(f"""
+    with st.spinner("Minting NFT..."):
+        if st.session_state.name and st.session_state.desc and st.session_state.mint_to_address:
+            cid = uploadNFT(st.session_state.img_url, st.session_state.prompt)
+            mint_info = mintNFT(st.session_state.name, st.session_state.desc, cid, st.session_state.mint_to_address)
+            st.success(f'NFT Minted successfully to {st.session_state.mint_to_address}')
+            res = json.loads(mint_info)
+            # st.json(res)
+            st.info(f"""
                          Chain: {res['chain']}\n
                          Transaction Hash: {res['transaction_hash']}\n
                          Transaction External URL: {res['transaction_external_url']}\n
                          Contract Address: {res['contract_address']}\n\n
                          """)
-                st.write("Check out your NFT on OpenSea [https://opensea.io/account/private](https://opensea.io/account/private)")
-            else:
-                st.error("Please enter the Name, Description and the Minting Wallet Address!")
-    except:
-        st.error('Oops! Seems like something went wrong. Try again!')
+            st.write(
+                "Check out your NFT on OpenSea [https://opensea.io/account/private](https://opensea.io/account/private)")
+        else:
+            st.error("Please enter the Name, Description and the Minting Wallet Address!")
 
 
 st.set_page_config(page_title="DaVinci - Generate & Mint NFTs with the help of AI within seconds", page_icon="🌄")
@@ -41,11 +40,12 @@ if st.button('Generate NFT'):
     time.sleep(5)
     st.success('NFT Generated Successfully!')
 
-
     with st.form(key='minting_form', clear_on_submit=False):
         name = st.text_input(label='', label_visibility='hidden', placeholder='Give your NFT a name', key='name')
-        desc = st.text_area(label='', label_visibility='hidden', placeholder='Write a short description about your NFT', key='desc')
-        mint_to_address = st.text_input(label='', label_visibility='hidden', placeholder='Minting Wallet Address', key='mint_to_address')
+        desc = st.text_area(label='', label_visibility='hidden', placeholder='Write a short description about your NFT',
+                            key='desc')
+        mint_to_address = st.text_input(label='', label_visibility='hidden', placeholder='Minting Wallet Address',
+                                        key='mint_to_address')
 
         col1, col2, col3, col4, col5 = st.columns(5)
         with col3:
